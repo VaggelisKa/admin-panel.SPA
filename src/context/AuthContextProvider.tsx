9c826e7ebfdd6369dbefd7ea011fa57c51b28664
Vaggelis Karavasileiadis <e.karavasileiadis@gmail.com>
2020-12-31 21:38:52 +0200

    Connecting client to api and creating user query

--- src/context/AuthContextProvider.tsx
+++ src/context/AuthContextProvider.tsx
@@ -1,4 +1,7 @@
 import React, { createContext, useState, useCallback, ReactChild } from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../apollo/queries';
+import { User } from '../types';
 
 interface Props {
   children: ReactChild | ReactChild[];
@@ -22,11 +25,13 @@ export const AuthContext = createContext<AuthContextValues>(initialState);
 
 const AuthContextProvider: React.FC<Props> = ({ children }: Props) => {
   const [authAction, setAuthAction] = useState<Actions>('close');
+  const { data } = useQuery<{user: User}>(QUERY_USER);
 
   const handleAuthAction: HandleAuthAction = useCallback((action) => {
     setAuthAction(action);
   }, []);
 
+  console.log('GQL data => ', data);
   return (
     <AuthContext.Provider
       value={{
